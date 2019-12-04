6799a85e2aa69cb8411f39c2a26964b4970199e9
pgardias <przemek.gardias@gmail.com>
2019-12-03 19:14:03 -0500

    Reverted data/archive content to commit 8eb3ccc

--- test_model.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-from model import *
-
-if __name__ == '__main__':
-    m = Model()
-    m.train()
-    exit(0)
---
+++ train_model.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+from model import *
+
+if __name__ == '__main__':
+    m = Model()
+    m.train()
